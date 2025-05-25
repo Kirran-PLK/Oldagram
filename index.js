0eaf596887022b6e6a41d9112be4479ca4369e1b
kirran <kirran231@gmail.com>
2025-05-25 17:18:01 +0530

    rendered using javascript objects

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const posts = [
         avatar: "images/avatar-vangogh.jpg",
         post: "images/post-vangogh.jpg",
         comment: "just took a few mushrooms lol",
-        likes: 21
+        likes: 21492
     },
     {
         name: "Gustave Courbet",
@@ -15,7 +15,7 @@ const posts = [
         avatar: "images/avatar-courbet.jpg",
         post: "images/post-courbet.jpg",
         comment: "i'm feelin a bit stressed tbh",
-        likes: 4
+        likes: 12502
     },
         {
         name: "Joseph Ducreux",
@@ -24,7 +24,51 @@ const posts = [
         avatar: "images/avatar-ducreux.jpg",
         post: "images/post-ducreux.jpg",
         comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
-        likes: 152
+        likes: 15137
     }
 ]
 
+
+const mainEl = document.getElementById("main-section")
+console.log(render(posts))
+
+
+if(posts){   
+    mainEl.innerHTML = render(posts)
+
+}
+
+function render(postsArray){
+    let postsElement = ""
+
+    for(let i=0; i<postsArray.length; i++)
+    {   
+        let arObj = postsArray[i]
+        postsElement+= ` 
+                <section>
+                    <div class="container">
+                        <div class="follower-details-wrapper">
+                            <img class="avatar-dimensions follower-avatar" src="${arObj.avatar}" alt="Follower avatar picture">
+                            <div class="follower-name-location-wrapper">
+                                <p class="follower-info follower-name">${arObj.name}</p>
+                                <p class="follower-info follower-location">${arObj.location}</p>
+                            </div>
+                        </div>
+                        <img class="post-img" src="${arObj.post}">
+                        <div class="follower-post-responses">
+                            <div class="follower-post-reactions">
+                                <img class="reactions" src="images/icon-heart.png">
+                                <img class="reactions" src="images/icon-comment.png">
+                                <img class="reactions" src="images/icon-dm.png">
+                            </div>
+                            <p>${arObj.likes} likes</p>
+                            <p>${arObj.username} <span>${arObj.comment}</span></p>
+                        </div>                  
+                    </div>
+            </section>
+            `
+            
+    }
+    
+    return postsElement
+}
